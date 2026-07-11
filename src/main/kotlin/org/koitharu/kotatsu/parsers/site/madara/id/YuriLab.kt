0d6c9fd41592fdd2c9d8054ddd86284f9ce8a441
bubz727 <sukaboba791@gmail.com>
2026-07-11 18:46:36 +0700

    Update YuriLab

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/YuriLab.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/YuriLab.kt
@@ -27,6 +27,14 @@ internal class YuriLab(context: MangaLoaderContext) :
         }
     }
 
+    override suspend fun getDetails(manga: Manga): Manga {
+        val doc = webClient.httpGet(manga.url.toAbsoluteUrl(domain)).parseHtml()
+        val author = doc.selectFirst(".author-content a, .manga-author a")?.textOrNull()
+        return super.getDetails(manga).copy(
+            authors = setOfNotNull(author).ifEmpty { manga.authors }
+        )
+    }
+
 
     override suspend fun fetchAvailableTags(): Set<MangaTag> {
         val url = "https://$domain/?s=&post_type=wp-manga"
@@ -106,7 +114,6 @@ internal class YuriLab(context: MangaLoaderContext) :
             t++
         }
 
-        // Assign sequential chapter numbers (reversed order = oldest first)
         return allChapters.reversed().mapIndexed { index, chapter ->
             chapter.copy(number = (index + 1).toFloat())
         }
